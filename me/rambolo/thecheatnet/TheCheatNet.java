a1f8bc44250c0afcaa59390a2931f8f981a51da7
Rambolo <iamrambolo@gmail.com>
2014-05-29 03:41:48 +0100

    Did literally fuck all

--- me/rambolo/thecheatnet/TheCheatNet.java
+++ me/rambolo/thecheatnet/TheCheatNet.java
@@ -1,15 +1,31 @@
 package me.rambolo.thecheatnet;
 
-/*
- * Copyright (c) 2014, Rambolo (iamrambolo@gmai.com)
- * All rights reserved.
- * 
- * Redistribution
- */
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
+ * <b>TheCheatNet is a plugin developed by, Rambolo, that will stop every single kind of mod/cheat/hack 
+ * that will give the player an unfair advantage over all of the other players on the server. 
+ * This plugin will ensure that every player will have to use a "non-vanilla" client 
+ * (this does not include any perfomance enhancing mods, e.g. Optifine).</b>
  * 
  * @author Rambolo
  */
-public class TheCheatNet {
 
+/*
+ * d8888888888b  dP                          ,ad8888ba,   dP                                             d88b      dP                       
+ *      88       88                         d8"'    `"8b  88                                     ,d      8888b     88                ,d     
+ *      88       88                        d8'            88                                     88      88 `8b    88                88     
+ *      88       88,dPPYba,    ,adPPYba,   88             88,dPPYba,    ,adPPYba,  ,adPPYYba,  MM88MMM   88  `8b   88   ,adPPYba,  MM88MMM  
+ *      88       88P'    "8a  a8P_____88   88             88P'    "8a  a8P_____88  ""     `Y8    88      88   `8b  88  a8P_____88    88     
+ *      88       88       88  8PP"""""""   Y8,            88       88  8PP"""""""  ,adPPPPP88    88      88    `8b 88  8PP"""""""    88     
+ *      88       88       88  "8b,   ,aa    Y8a.    .a8P  88       88  "8b,   ,aa  88,    ,88    88,     88     `8888  "8b,   ,aa    88,    
+ *      88       dP       dP   `"Ybbd8"'     `"Y8888Y"'   dP       dP   `"Ybbd8"'  `"8bbdP"Y8    "Y888   dP      `88P   `"Ybbd8"'    "Y888  
+ */
+/**
+ * 
+ */
+public class TheCheatNet extends JavaPlugin{
+	
+	
+	
 }
---
+++ me/rambolo/thecheatnet/api/TheCheatNetAPI.java
@@ -0,0 +1,5 @@
+package me.rambolo.thecheatnet.api;
+
+public class TheCheatNetAPI {
+
+}
